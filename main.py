ffde70f838de7fbff88a6484266efc5ac4230866
ADARSH PRADHAN <77764743+adarsh-pradhan@users.noreply.github.com>
2024-01-09 09:55:37 +0530

    Update main.py
    changed removed host and port details from the line "app.run"

--- main.py
+++ main.py
@@ -1727,4 +1727,4 @@ if __name__ == "__main__":
 
     # To allow access for other users in local network
     # app.run(host='0.0.0.0', port=5000, debug=True)
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(debug=False)
